1c5a9f4f2ae984f59e5a5b2737e5f72b91b86847
HK12390 <hanna.w.khan@gmail.com>
2021-09-04 19:26:53 -0700

    First Commit

---
+++ src/firstcode/main.java
@@ -0,0 +1,34 @@
+package firstcode;
+
+class main {
+    public static void main(String[] args) {
+        int[] groot = {0, 1, 2, 3, 4};
+
+        int[] bar = new int[100];
+        for (int i = 0; i < 100; i++) {
+            bar[i] = i;
+        }
+        System.out.println("The number of even numbers in groot is:");
+        System.out.println(countEvenNum(groot, 5));
+
+        System.out.println("The number of even numbers in bar is:");
+        System.out.println(countEvenNum(bar, 100));
+
+    }
+
+
+    private static int countEvenNum(int[] arr, int size) {
+        int count = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] % 2 == 0) {
+                count++;
+            } else {
+
+            }
+        }
+
+        return count;
+    }
+
+}
